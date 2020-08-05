f3ee5c1793fe3e48c52d3f466167948f929898ce
Dmitry Zapashchikov <dmitriiz@hotmail.com>
2020-08-05 12:58:13 +0300

    Initial commit

---
+++ src/native-test/java/com/use2go/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.use2go;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
